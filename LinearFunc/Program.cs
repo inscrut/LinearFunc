2cb32179c549fa1f1a112ac317c7cc32102b1a1f
Kirill S. <kirr2@gf.tom.ru>
2015-12-13 10:45:03 +0600

    Release commit

---
+++ LinearFunc/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            linfunc liner = new linfunc(0, 0, 1, 1);
+
+            if(liner.check(2,2)) System.Console.Write("Belong");
+            else System.Console.Write("Not Belong");
+        }
+
+        class linfunc
+        {
+            public float k { get { return kb; } }
+            public float b { get { return bb; } }
+
+            float kb, bb;
+
+            public linfunc(int x1, int y1, int x2, int y2)
+            {
+                kb = (float)(y1 - y2) / (float)(x1 - x2);
+                bb = (float)(y1 - kb * x1);
+            }
+
+            public bool check(int x, int y) //Belonging to the point function
+            {
+                // y = kx + b
+                if (y == kb * x + bb) return true;
+                else return false;
+            }
+        }
+    }
+}
---
+++ LinearFunc/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("LinearFunc")]
+[assembly: AssemblyDescription("Linear function")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Home")]
+[assembly: AssemblyProduct("LinearFunc")]
+[assembly: AssemblyCopyright("Copyright © Home 2015")]
+[assembly: AssemblyTrademark("GF")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("1c95b458-922a-4dff-9dd7-3cdd54d03a93")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
